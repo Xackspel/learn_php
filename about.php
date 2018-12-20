c40ba2ab9cb25a6d9ec89a031bcb4f24763765b0
Xackspel <42944802+Xackspel@users.noreply.github.com>
2018-12-20 11:52:48 +0200

    $_POST

--- about.php
+++ about.php
@@ -1,8 +1,8 @@
 <?php
     error_reporting(E_ALL);
     var_dump($_POST);
-    if (isset($_GET["name"])) {
-        echo $_GET["name"];
+    if (isset($_POST["myname"])) {
+        echo $_POST["myname"];
     }
     else{
         echo "Empty";
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<form action="about.php" method="POST">
-    <input name="myname" type="text"><br>
-    <button>Confirm</button>
-</form>
+<?php
+    $template = '<form action="about.php" method="POST">
+        <input name="myname" type="text"><br>
+        <button>Confirm</button>
+    </form>';
+    echo $template;
+
+    var_dump($_SERVER);
+?>
