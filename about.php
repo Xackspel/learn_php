e3fb66a23b1550f008dd955696d1d3b6b11e40c1
Xackspel <42944802+Xackspel@users.noreply.github.com>
2018-12-20 14:37:08 +0200

    phpmadmin

--- about.php
+++ about.php
@@ -1,10 +1,4 @@
 <?php
-    error_reporting(E_ALL);
-    var_dump($_POST);
-    if (isset($_POST["myname"])) {
-        echo $_POST["myname"];
-    }
-    else{
-        echo "Empty";
-    }
+    session_start();
+    echo $_SESSION["Name"];
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,36 @@
 <?php
-    $template = '<form action="about.php" method="POST">
-        <input name="myname" type="text"><br>
-        <button>Confirm</button>
-    </form>';
-    echo $template;
+    session_start();
 
-    var_dump($_SERVER);
+    $posts = [
+        [
+            "title" => "First steps in learning language",
+            "desc" => "Lorem ipsum dolor sit amet, volutpat pertinacia interpretaris"
+        ],
+        [
+            "title" => "Second steps in learning language",
+            "desc" => "Lorem ipsum dolor sit amet, volutpat pertinacia interpretaris"
+        ],
+        [
+            "title" => "Third steps in learning language",
+            "desc" => "Lorem ipsum dolor sit amet, volutpat pertinacia interpretaris"
+        ],
+
+    ];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <?php foreach($posts as $post){echo $post};?>
+        </div>
+    </div>
+</body>
+</html>
